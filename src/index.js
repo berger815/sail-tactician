57be29c76f295b04f5ae10c7669e146d2fb6afff
berger815 <129002430+berger815@users.noreply.github.com>
2026-07-12 23:49:56 -0400

    Integrate start-line core: update src/index.js

--- src/index.js
+++ src/index.js
@@ -3,5 +3,6 @@ export * from './core/confidence.js';
 export * from './core/geo.js';
 export * from './core/polars.js';
 export * from './core/start-line.js';
+export * from './adapters/legacy-start.js';
 export * from './services/gps.js';
 export * from './state/store.js';
